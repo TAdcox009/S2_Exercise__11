9c160beffc664f564e5fd5e3ba60c862ed28a5bc
Unknown <TAdcox009@west-mec.org>
2019-02-05 18:15:22 -0700

    commit 1
    Renamed files, added name and date, added strict mode into the JavaScript file. Tested a function in JavaScript. Linked a JavaScript file in the HTML.

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,12 @@
+"use stirct";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+     Author: Todd Adcox Jr
+     Date:   2-5-19  
 
 */
-
